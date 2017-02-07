7421c182fbc576cd883533797586f3fae0d33d69
Amos Onn <amosonn@gmail.com>
2017-02-07 03:54:56 +0100

    Added size, fixed the last run to match size.

--- src/byte_runs.rs
+++ src/byte_runs.rs
@@ -8,17 +8,21 @@ pub struct ByteRun {
 
 pub struct ByteRunsRef {
     runs: Box<[ByteRun]>,
+    size: u64,
     pos: u64,
     cur_run: usize,
     offset_in_run: u64,
 }
 
 impl ByteRunsRef {
-    pub fn new<T>(it: T) -> Self where T: Iterator<Item=ByteRun> {
+    pub fn new<T>(size: u64, it: T) -> Self where T: Iterator<Item=ByteRun> {
         let mut runs: Vec<ByteRun> = it.collect();
+        let gross_size = &runs.iter().map(|br| br.len).sum();
+        runs.split_last_mut().unwrap().0.len -= gross_size - size;
         runs.sort();
         ByteRunsRef {
             runs: runs.into_boxed_slice(),
+            size: size,
             pos: 0,
             cur_run: 0,
             offset_in_run: 0,
