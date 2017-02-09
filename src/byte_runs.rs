41dee682e4ba408e3c2a61a46b1d35e688ae6786
Amos Onn <amosonn@gmail.com>
2017-02-09 18:36:58 +0100

    Implemented seek for byte_runs.

--- src/byte_runs.rs
+++ src/byte_runs.rs
@@ -1,4 +1,7 @@
 
+use std::io::{Seek, SeekFrom};
+use std::io;
+
 #[derive(PartialEq, Eq, PartialOrd, Ord)]
 pub struct ByteRun {
     file_offset: u64,
@@ -26,6 +29,41 @@ impl ByteRunsRef {
             pos: 0,
             cur_run: 0,
             offset_in_run: 0,
+        }
+    }
+
+    fn set_pos(&mut self, pos: u64) -> io::Result<u64> {
+        self.pos = pos;
+        if pos > self.size {
+            self.cur_run = self.runs.len();
+            self.offset_in_run = 0;
+        } else {
+            self.cur_run = match self.runs.binary_search_by_key(&pos, |br| br.file_offset) {
+                Ok(x) => x,  // We're at the beginning of this slice.
+                Err(x) => x-1,  // We could be inserted after this slice, which
+                                // means we're somewhere within it.
+            };
+            self.offset_in_run = self.pos - self.runs[self.cur_run].file_offset;
+        }
+        Ok(pos)
+    }
+}
+
+impl Seek for ByteRunsRef {
+    fn seek(&mut self, pos: SeekFrom) -> io::Result<u64> {
+        let (base_pos, offset) = match pos {
+            SeekFrom::Start(x) => { return self.set_pos(x); }
+            SeekFrom::Current(x) => (self.pos, x),
+            SeekFrom::End(x) => (self.size, x),
+        };
+        let new_pos = if offset >= 0 {
+            base_pos.checked_add(offset as u64)
+        } else {
+            base_pos.checked_sub((offset.wrapping_neg()) as u64)
+        };
+        match new_pos {
+            Some(x) => self.set_pos(x),
+            None => Err(io::Error::new(io::ErrorKind::InvalidInput, "Bad seek pos.")),
         }
     }
 }
