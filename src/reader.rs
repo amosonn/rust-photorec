d258a6e75d312d0ac8eab6c793ffdfa5167097b3
Amos Onn <amosonn@gmail.com>
2017-02-11 20:53:52 +0100

    Generizd ByteRunsReader.

--- src/reader.rs
+++ src/reader.rs
@@ -1,14 +1,13 @@
 
 use std::io::{Read, Seek, SeekFrom};
 use std::io;
-use std::fs::File;
 
 use super::byte_runs::ByteRunsRef;
 
 
-pub struct ByteRunsReader {
+pub struct ByteRunsReader<R> {
     brf: ByteRunsRef,
-    file: File,
+    inner: R,
 }
 
 //impl ByteRunsReader {
@@ -18,18 +17,18 @@ pub struct ByteRunsReader {
     //}
 
 
-impl Seek for ByteRunsReader {
+impl<R> Seek for ByteRunsReader<R> {
     fn seek(&mut self, pos: SeekFrom) -> io::Result<u64> { self.brf.seek(pos) }
 }
 
 
-impl Read for ByteRunsReader {
+impl<R: Read+Seek> Read for ByteRunsReader<R> {
     fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
         let desc = self.brf.desc_read(buf.len());
         if desc.len == 0 { return Ok(0); }
         // FIXME types.
         let buf2 = &mut buf[..(desc.len as usize)];
         // FIXME if read is too short, brf is out of sync.
-        self.file.seek(SeekFrom::Start(desc.disk_pos)).and_then(|_| self.file.read(buf2))
+        self.inner.seek(SeekFrom::Start(desc.disk_pos)).and_then(|_| self.inner.read(buf2))
     }
 }
