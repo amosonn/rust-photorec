dd851af64120163f7c13134c021f9018dce0cf19
Amos Onn <amosonn@gmail.com>
2020-06-04 00:32:08 +0200

    Thicklen report.rs tests, add BadChildName

--- src/report.rs
+++ src/report.rs
@@ -23,10 +23,12 @@ type Result<T> = std::result::Result<T, ReportXmlError>;
 pub enum ReportXmlError {
     #[error("Error parsing: {0}")]
     Parse(#[from] ParseError),
-    #[error("Missing field {0} in xml")]
-    MissingField(&'static str),
+    #[error("Missing field {field_name} in xml")]
+    MissingField { field_name: &'static str },
     #[error("Missing text in field {field_name} in xml")]
     MissingText { field_name: String },
+    #[error("Unexpected child of name {field_name} in xml")]
+    BadChildName { expected_name: &'static str, field_name: String },
     #[error("Missing attr {attr_name} in field {field_name} in xml")]
     MissingAttr { attr_name: &'static str, field_name: String },
     #[error("Malformed text in field {field_name} in xml, parse error: {source}")]
@@ -38,7 +40,7 @@ pub enum ReportXmlError {
 }
 
 fn get_child<'a>(elem: &'a Element, name: &'static str) -> Result<&'a Element> {
-    elem.get_child(name).ok_or(ReportXmlError::MissingField(name))
+    elem.get_child(name).ok_or(ReportXmlError::MissingField { field_name: name })
 }
 
 fn get_text<'a>(elem: &'a Element) -> Result<&'a String> {
@@ -55,11 +57,21 @@ fn get_attr_number<'a>(elem: &'a Element, name: &'static str) -> Result<u64> {
     x.parse::<u64>().map_err(|e| ReportXmlError::MalformedAttr { attr_name: name, field_name: elem.name.clone(), source: e })
 }
 
+fn assert_name<'a>(elem: &'a Element, name: &'static str) -> Result<()> {
+    if elem.name != name {
+        Err(ReportXmlError::BadChildName { expected_name: name, field_name: elem.name.clone() })
+    } else {
+        Ok(())
+    }
+}
+
+
 fn to_byte_runs_ref(elem: Element) -> Result<(String, ByteRunsRef)> {
     let name = get_text(get_child(&elem, "filename")?)?.clone();
     let size = get_number(get_child(&elem, "filesize")?)?;
     let byte_runs = get_child(&elem, "byte_runs")?.children.iter()
         .map(|x| -> Result<ByteRun> {
+            assert_name(x, "byte_run")?;
             let file_offset = get_attr_number(x, "offset")?;
             let disk_pos = get_attr_number(x, "img_offset")?;
             let len = get_attr_number(x, "len")?;
@@ -149,10 +161,6 @@ fn test_report_xml_parse() {
     </byte_runs>
   </fileobject>
   <fileobject>
-    <filename>f140247350_assets.zip</filename>
-    <filesize>10499571</filesize>
-    <byte_runs>
-    </byte_runs>
   </fileobject>
   <fileobject>
     <filename>f140197124_res.zip</filename>
@@ -167,12 +175,246 @@ fn test_report_xml_parse() {
     assert_eq!(rx.image_filename(), "/dev/sdb");
     let e = rx.next().unwrap().unwrap();
     assert_eq!(e.0, "f140247350_assets.zip");
-    let e = rx.next().unwrap().err().unwrap();
-    assert_let!(ReportXmlError::BadByteRunsRef { file_name: x, source: e } = e, {
-        assert_eq!(x, "f140247350_assets.zip");
-        assert_let!(ByteRunsRefError::Empty = e);
-    });
+    // We only check errors are handled and iteration continues
+    let _e = rx.next().unwrap().err().unwrap();
     let e = rx.next().unwrap().unwrap();
     assert_eq!(e.0, "f140197124_res.zip");
     assert!(rx.next().is_none());
+}
+
+#[test]
+fn test_report_xml_parse_errors() {
+    let s = r##"<?xml version='1.0' encoding='UTF-8'?>
+<dfxml xmloutputversion='1.0'"##;
+    let rx_err = ReportXml::parse(s.as_bytes());
+    assert_let!(Err(ReportXmlError::Parse(_)) = rx_err);
+
+    let s = r##"<?xml version='1.0' encoding='UTF-8'?>
+<dfxml xmloutputversion='1.0'>
+</dfxml>"##;
+    let rx_err = ReportXml::parse(s.as_bytes());
+    assert_let!(Err(ReportXmlError::MissingField { field_name: ref s }) = rx_err, {
+        assert_eq!(*s, "source");
+    });
+
+    let s = r##"<?xml version='1.0' encoding='UTF-8'?>
+<dfxml xmloutputversion='1.0'>
+  <source>
+  </source>
+</dfxml>"##;
+    let rx_err = ReportXml::parse(s.as_bytes());
+    assert_let!(Err(ReportXmlError::MissingField { field_name: ref s }) = rx_err, {
+        assert_eq!(*s, "image_filename");
+    });
+
+    let s = r##"<?xml version='1.0' encoding='UTF-8'?>
+<dfxml xmloutputversion='1.0'>
+  <source>
+    <image_filename />
+  </source>
+</dfxml>"##;
+    let rx_err = ReportXml::parse(s.as_bytes());
+    assert_let!(Err(ReportXmlError::MissingText { field_name: ref s }) = rx_err, {
+        assert_eq!(*s, "image_filename");
+    });
+}
+
+#[test]
+fn test_report_xml_iter_errors() {
+    let s = r##"<?xml version='1.0' encoding='UTF-8'?>
+<dfxml xmloutputversion='1.0'>
+  <metadata 
+  xmlns='http://www.forensicswiki.org/wiki/Category:Digital_Forensics_XML' 
+  xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' 
+  xmlns:dc='http://purl.org/dc/elements/1.1/'>
+    <dc:type>Carve Report</dc:type>
+  </metadata>
+  <creator>
+    <package>PhotoRec</package>
+    <version>7.1-WIP</version>
+    <build_environment>
+      <compiler>GCC 6.2</compiler>
+      <library name='libext2fs' version='1.2.3'/>
+      <library name='libewf' version='none'/>
+      <library name='libjpeg' version='libjpeg-turbo-1.2.3'/>
+      <library name='libntfs' version='libntfs-3g'/>
+      <library name='zlib' version='1.2.3'/>
+    </build_environment>
+    <execution_environment>
+      <os_sysname>Linux</os_sysname>
+      <os_release>4.5.6</os_release>
+      <os_version>Version</os_version>
+      <host>test</host>
+      <arch>x86_64</arch>
+      <uid>0</uid>
+      <start_time>2017-02-19T02:02:21+0100</start_time>
+    </execution_environment>
+  </creator>
+  <source>
+    <image_filename>/dev/sdb</image_filename>
+    <sectorsize>512</sectorsize>
+    <device_model>Generic STORAGE DEVICE</device_model>
+    <image_size>123456000000</image_size>
+    <volume>
+      <byte_runs>
+        <byte_run offset='0' img_offset='16777216' len='123439222784'/>
+      </byte_runs>
+    </volume>
+  </source>
+  <configuration>
+  </configuration>
+  <fileobject />
+  <fileobject>
+    <filesize>123</filesize>
+    <byte_runs>
+      <byte_run offset='0' img_offset='1' len='2'/>
+    </byte_runs>
+    <byte_runs />
+  </fileobject>
+  <fileobject>
+    <filename>f1</filename>
+    <byte_runs>
+      <byte_run offset='0' img_offset='1' len='2'/>
+    </byte_runs>
+  </fileobject>
+  <fileobject>
+    <filename>f2</filename>
+    <filesize>not-a-number</filesize>
+    <byte_runs>
+      <byte_run offset='0' img_offset='1' len='2'/>
+    </byte_runs>
+  </fileobject>
+  <fileobject>
+    <filename>f2</filename>
+    <filesize>10499571</filesize>
+  </fileobject>
+  <fileobject>
+    <filename>f3</filename>
+    <filesize>10499571</filesize>
+    <byte_runs />
+  </fileobject>
+  <fileobject>
+    <filename>f4</filename>
+    <filesize>10499571</filesize>
+    <byte_runs>
+      <bad_name />
+    </byte_runs>
+  </fileobject>
+  <fileobject>
+    <filename>f4</filename>
+    <filesize>10499571</filesize>
+    <byte_runs>
+      <byte_run img_offset='16777216' len='123439222784'/>
+    </byte_runs>
+  </fileobject>
+  <fileobject>
+    <filename>f4</filename>
+    <filesize>10499571</filesize>
+    <byte_runs>
+      <byte_run offset='nan' img_offset='16777216' len='123439222784'/>
+    </byte_runs>
+  </fileobject>
+  <fileobject>
+    <filename>f4</filename>
+    <filesize>10499571</filesize>
+    <byte_runs>
+      <byte_run offset='0' len='123439222784'/>
+    </byte_runs>
+  </fileobject>
+  <fileobject>
+    <filename>f4</filename>
+    <filesize>10499571</filesize>
+    <byte_runs>
+      <byte_run offset='0' img_offset='nan' len='123439222784'/>
+    </byte_runs>
+  </fileobject>
+  <fileobject>
+    <filename>f4</filename>
+    <filesize>10499571</filesize>
+    <byte_runs>
+      <byte_run offset='0' img_offset='16777216'/>
+    </byte_runs>
+  </fileobject>
+  <fileobject>
+    <filename>f4</filename>
+    <filesize>10499571</filesize>
+    <byte_runs>
+      <byte_run offset='0' img_offset='16777216' len='nan'/>
+    </byte_runs>
+  </fileobject>
+  <fileobject>
+    <filename>f4</filename>
+    <filesize>10499571</filesize>
+    <byte_runs>
+      <byte_run offset='0' img_offset='16777216' len='123439222784'/>
+      <bad_name />
+    </byte_runs>
+  </fileobject>
+</dfxml>"##;
+    let mut rx = ReportXml::parse(s.as_bytes()).unwrap();
+    let e = rx.next().unwrap().err().unwrap();
+    assert_let!(ReportXmlError::MissingField { field_name: ref s } = e, {
+        assert_eq!(*s, "filename");
+    });
+    let e = rx.next().unwrap().err().unwrap();
+    assert_let!(ReportXmlError::MissingField { field_name: ref s } = e, {
+        assert_eq!(*s, "filename");
+    });
+    let e = rx.next().unwrap().err().unwrap();
+    assert_let!(ReportXmlError::MissingField { field_name: ref s } = e, {
+        assert_eq!(*s, "filesize");
+    });
+    let e = rx.next().unwrap().err().unwrap();
+    assert_let!(ReportXmlError::MalformedText { field_name: ref s, source: _ } = e, {
+        assert_eq!(*s, "filesize");
+    });
+    let e = rx.next().unwrap().err().unwrap();
+    assert_let!(ReportXmlError::MissingField { field_name: ref s } = e, {
+        assert_eq!(*s, "byte_runs");
+    });
+    let e = rx.next().unwrap().err().unwrap();
+    assert_let!(ReportXmlError::BadByteRunsRef { file_name: x, source: e } = e, {
+        assert_eq!(x, "f3");
+        assert_let!(ByteRunsRefError::Empty = e);
+    });
+    let e = rx.next().unwrap().err().unwrap();
+    assert_let!(ReportXmlError::BadChildName { expected_name: ref exp, field_name: ref field } = e, {
+        assert_eq!(*exp, "byte_run");
+        assert_eq!(*field, "bad_name");
+    });
+    let e = rx.next().unwrap().err().unwrap();
+    assert_let!(ReportXmlError::MissingAttr { attr_name: ref a, field_name: ref f } = e, {
+        assert_eq!(*a, "offset");
+        assert_eq!(*f, "byte_run");
+    });
+    let e = rx.next().unwrap().err().unwrap();
+    assert_let!(ReportXmlError::MalformedAttr { attr_name: ref a, field_name: ref f, source: _ } = e, {
+        assert_eq!(*a, "offset");
+        assert_eq!(*f, "byte_run");
+    });
+    let e = rx.next().unwrap().err().unwrap();
+    assert_let!(ReportXmlError::MissingAttr { attr_name: ref a, field_name: ref f } = e, {
+        assert_eq!(*a, "img_offset");
+        assert_eq!(*f, "byte_run");
+    });
+    let e = rx.next().unwrap().err().unwrap();
+    assert_let!(ReportXmlError::MalformedAttr { attr_name: ref a, field_name: ref f, source: _ } = e, {
+        assert_eq!(*a, "img_offset");
+        assert_eq!(*f, "byte_run");
+    });
+    let e = rx.next().unwrap().err().unwrap();
+    assert_let!(ReportXmlError::MissingAttr { attr_name: ref a, field_name: ref f } = e, {
+        assert_eq!(*a, "len");
+        assert_eq!(*f, "byte_run");
+    });
+    let e = rx.next().unwrap().err().unwrap();
+    assert_let!(ReportXmlError::MalformedAttr { attr_name: ref a, field_name: ref f, source: _ } = e, {
+        assert_eq!(*a, "len");
+        assert_eq!(*f, "byte_run");
+    });
+    let e = rx.next().unwrap().err().unwrap();
+    assert_let!(ReportXmlError::BadChildName { expected_name: ref exp, field_name: ref field } = e, {
+        assert_eq!(*exp, "byte_run");
+        assert_eq!(*field, "bad_name");
+    });
 }
