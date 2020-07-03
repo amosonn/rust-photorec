110081cc9d0468b8cae5cc72d0c33dbfab8a9fe7
Amos Onn <amosonn@gmail.com>
2020-07-04 00:47:44 +0200

    Make photorec-size ignore jpgs even on error

--- src/bin/photorec-size.rs
+++ src/bin/photorec-size.rs
@@ -1,7 +1,7 @@
 use std::env::args_os;
 use std::fs::File;
 
-use photorec::ReportXml;
+use photorec::{ReportXml, ReportXmlError, FileDescriptionError};
 
 fn main() {
     let reports = args_os().skip(1).map(|fname| {
@@ -13,12 +13,28 @@ fn main() {
     }).collect::<Vec<_>>();
     for (fname, report) in reports.iter() {
         let size = report.iter().filter_map(|x| {
-            let (s, fd) = x.unwrap();
-            if s.ends_with(".jpg") { Some(fd.size()) } else { None }
+            match x {
+                Ok((s, fd)) => if s.ends_with(".jpg") { Some(fd.size()) } else { None }
+                Err(ReportXmlError::BadFileDescription { file_name: ref s, source: ref e }) => {
+                    if s.ends_with(".jpg") { match e {
+                        FileDescriptionError::Empty => None,
+                        _ => { x.unwrap(); unreachable!() } // We panic anyway
+                    } } else { None } 
+                }
+                _ => { x.unwrap(); unreachable!() } // We panic anyway
+            }
         }).fold(0, |a, b| a + b);
         let count = report.iter().filter_map(|x| {
-            let (s, fd) = x.unwrap();
-            if s.ends_with(".jpg") { Some(fd.size()) } else { None }
+            match x {
+                Ok((s, _)) => if s.ends_with(".jpg") { Some(()) } else { None }
+                Err(ReportXmlError::BadFileDescription { file_name: ref s, source: ref e }) => {
+                    if s.ends_with(".jpg") { match e {
+                        FileDescriptionError::Empty => None,
+                        _ => { x.unwrap(); unreachable!() } // We panic anyway
+                    } } else { None } 
+                }
+                _ => { x.unwrap(); unreachable!() } // We panic anyway
+            }
         }).count();
         println!("{}: {} entries, {} bytes", fname, count, size);
     }
